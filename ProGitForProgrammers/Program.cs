243edc8c1da6c0a99ec95f5b95d3bbf8720360df
matpea82 <136901278+matpea82@users.noreply.github.com>
2023-06-20 23:16:22 +0700

    My message

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in Visual Studio");
 Console.WriteLine("I just added this in the command line repo");
+Console.WriteLine("This line added to show the staging area");
